1cdd1411bdf49880ee2a1151a972016b5d13672e
niklas <nikl2513@outlook.dk>
2022-09-19 13:23:24 +0200

    4. commit

---
+++ src/dato.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class dato {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indtast år: ");
+        int aar = sc.nextInt();
+        System.out.println("Indtast måned: ");
+        int maaned = sc.nextInt();
+
+    }
+}
